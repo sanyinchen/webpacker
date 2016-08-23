a513374dbaaad2ff18ca9f754bedf0e043770317
sanyinchen <1554194922@qq.com>
2016-08-23 22:42:39 +0800

    init

---
+++ app/component.js
@@ -0,0 +1,11 @@
+/**
+ * Created by sanyinchen on 16/8/23.
+ */
+'use strict'
+
+module.exports = function () {
+    var element = document.createElement('h1');
+
+    element.innerHTML = 'Hello Sanyinchen';
+    return element;
+}
---
+++ app/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by sanyinchen on 16/8/23.
+ */
+'use strict';
+var component=require('./component.js');
+
+document.body.appendChild(component());
